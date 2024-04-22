2c0534e7e813c189588ba31e3e2c8ee4476e991f
Joseba Iturregui <75678178+josebaiturregui@users.noreply.github.com>
2024-04-22 21:05:46 +0200

    commit

--- arrancar-proyecto.bat
+++ arrancar-proyecto.bat
@@ -1,2 +1,2 @@
 start cmd.exe /k "cd C:\Program Files\MongoDB\Server\6.0\bin && mongod.exe --dbpath C:\data6\db"
-start cmd.exe /k "cd C:\Users\joseb\TFM_posgrado\TFM_Backend\TFM_Joseba_Yoanyer_BackEnd\api-rest-red-social && npm start"
+start cmd.exe /k "cd C:\Users\joseb\TFM_posgrado\TFM_Backend\TFM_Joseba_Yoanyer_BackEnd && npm start"
